c8fe61429a7311e8eded6de3c672a1a9a23d4bfe
6y6a <gubinalexeymain@gmail.com>
2018-06-12 16:23:36 +0300

    Добавлены глобальные стили приложения

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,91 @@
 <template>
   <div id="app">
+    <div id="details">
+      <h1>Tic Tac Toe</h1>
+    </div>
 
+    <board />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data () {
-    return {
+  import board from './components/Board.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      board
+    },
+    data() {
+      return {}
     }
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
+  body {
+    background-color: #fff;
+    color: #fff;
+    font-family: 'Dosis', Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin: 0;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  #app {
+    margin: 0 auto;
+    max-width: 270px;
+    color: #34495e;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  h1 {
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 3em;
+  }
 
-a {
-  color: #42b983;
-}
+  .restart {
+    background-color: #e74c3c;
+    color: #fff;
+    border: 0;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+    font-family: 'Dosis', Helvetica, sans-serif;
+    font-size: 1.4em;
+    font-weight: bold;
+    margin: 0;
+    padding: 15px;
+    width: 100%;
+
+    &:hover {
+      background-color: #c0392b;
+      cursor: pointer;
+    }
+  }
+
+  .scoreBoard {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    height: 15px;
+    background-color: #16a085;
+    box-shadow: 10px solid #fff;
+    padding: 20px;
+    overflow-x: none;
+
+    h2 {
+      margin: 0;
+    }
+
+    span {
+      float: right;
+      font-size: 1.5em;
+      font-weight: bold;
+      margin-left: 20px;
+    }
+  }
+
 </style>
